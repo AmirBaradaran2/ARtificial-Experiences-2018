d5816160a5a76801b1dfa42404d29c44362bc703
He Li <jet.in.brain@gmail.com>
2016-12-08 03:23:26 -0500

    add greeting, fix video dismatch

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject soundManager;
+
+	void Awake () {
+		if (soundManager && SoundManager.instance == null)
+			Instantiate(soundManager);
+	}
+}
---
+++ Assets/Scripts/SceneButtonTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneButtonTrigger : MonoBehaviour {
+
+	public void changeScene(string sceneName) {
+		SceneManager.LoadScene (sceneName);
+	}
+}
