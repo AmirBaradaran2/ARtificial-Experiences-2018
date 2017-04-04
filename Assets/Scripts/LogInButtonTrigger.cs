809037ff73f93bec9238a64560f2e5411967d7d1
Julie <julie.y.chien@gmail.com>
2017-04-03 20:26:01 -0400

    fixed bug with fb login

--- Assets/Scripts/LogInButtonTrigger.cs
+++ Assets/Scripts/LogInButtonTrigger.cs
@@ -4,26 +4,32 @@ using Facebook.Unity;
 
 public class LogInButtonTrigger : MonoBehaviour {
 
-	public void logIn() {
+	void Awake () {
+		Debug.Log ("loginbuttontrigger awake called");
 		Debug.Log ("initializing fb");
 		if (!FB.IsInitialized) {
 			// Initialize the Facebook SDK
 			FB.Init (InitCallback);
 		} else {
 			// Already initialized, signal an app activation App Event
+			Debug.Log("awake: fb already initialized");
 			FB.ActivateApp ();
 		}
 
-		Debug.Log ("fb initialized, requesting login");
+	}
+
+
+	public void logIn() {
+		Debug.Log ("requesting login");
 		var perms = new List<string>(){"public_profile", "email"};
 		FB.LogInWithReadPermissions(perms, AuthCallback);
-
-
 	}
 
 	private void InitCallback ()
 	{
 		if (FB.IsInitialized) {
+			Debug.Log ("initcallback: fb successfully initialized, activating app");
+
 			// Signal an app activation App Event
 			FB.ActivateApp();
 			// Continue with Facebook SDK
